8cfbd9108f1fa6745fed3f76dace2be6fd50927a
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-03-02 20:47:55 -0300

    finalizad

--- src/Middlewares/validatedShortUrl.js
+++ src/Middlewares/validatedShortUrl.js
@@ -1,18 +1,25 @@
 import db from "../Config/db.js";
 
-export const validateShortUrl = async (req, res, next)=> {
+export const validateShortUrl = async (req, res, next) => {
 	const { shortUrl } = req.params;
-	
+
 	try {
-		const url = await db.query(`SELECT * FROM shortens WHERE short_url = $1`,[shortUrl]);
-		console.log(url.rowCount)
-		if (url.rowCount === 1) {
-			await db.query('UPDATE visits SET visti = visti + 1 WHERE short_id = $1',[url.rows[0].id]);
-			res.locals.url = url.rows[0];
-			next();
-		}else{
-			return res.status(404).send("Not Found");
+		const result = await db.query(
+			'SELECT url FROM shortens WHERE short_url = $1',
+			[shortUrl]
+		);
+		if (result.rows.length === 0) {
+			res.status(404).send('URL not found');
+			return;
 		}
+
+		await db.query(
+			'INSERT INTO visits (short_id, visti) VALUES ((SELECT id FROM shortens WHERE short_url = $1), 1)',
+			[shortUrl]
+		);
+
+		res.locals.url = result.rows[0]
+		next();
 
 	} catch (error) {
 		res.status(500).send(error.message);
